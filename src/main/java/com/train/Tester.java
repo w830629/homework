65c3c46b79cac74a1b0599c34d4ab7a7b58e9aae
Winnie Kang <o922887715@gmail.com>
2021-08-26 15:13:47 +0800

    homework 2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number of tickets:");
-        int tics = scanner.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        int roundtrip = scanner.nextInt();
-        Ticket ticket = new Ticket(tics,roundtrip);
-        ticket.show();
-
+        int tickets = 0;
+        while ( tickets != -1) {
+            System.out.println("Please enter number of tickets:");
+            tickets = scanner.nextInt();
+            if (tickets == -1){
+                break;
+            }
+            System.out.println("How many round-trip tickets: ");
+            int roundtrip = scanner.nextInt();
+            Ticket ticket = new Ticket(tickets, roundtrip);
+            ticket.show();
+        }
     }
-
-
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -2,16 +2,20 @@ package com.train
 import java.util.*
 
 fun main(Args: Array<String>){
-
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets:")
-    val tickets = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    val roundTrip = scanner.nextInt()
+    var tickets = 0
+    while (tickets!= -1) {
+        print("Please enter number of tickets:")
+        tickets = scanner.nextInt()
+        if (tickets == -1){
+            break
+        }
+        print("How many round-trip tickets: ")
+        val roundTrip = scanner.nextInt()
+        var tic = TrainTicket(tickets, roundTrip)
+        tic.show()
 
-    var tic = TrainTicket(tickets ,roundTrip)
-    tic.show()
-
+    }
 }
 
 class TrainTicket(var tickets: Int, var roundTrip: Int) {
